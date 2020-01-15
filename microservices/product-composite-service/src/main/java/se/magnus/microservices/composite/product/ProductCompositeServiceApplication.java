c9647054b112679ee58f7a25b6a94bf6e4ebb50d
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-01-16 00:39:13 +0700

    Remove Swagger

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,23 +1,26 @@
 package se.magnus.microservices.composite.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
+import se.magnus.microservices.composite.product.services.ProductCompositeIntegration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+import java.util.LinkedHashMap;
 
 import static java.util.Collections.emptyList;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
-@EnableSwagger2WebFlux
+//@EnableSwagger2WebFlux
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
@@ -60,9 +63,22 @@ public class ProductCompositeServiceApplication {
                 ));
     }
 
+	@Autowired
+	HealthAggregator healthAggregator;
+
+	@Autowired
+	ProductCompositeIntegration integration;
+
 	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
+	ReactiveHealthIndicator coreServices() {
+
+		ReactiveHealthIndicatorRegistry registry = new DefaultReactiveHealthIndicatorRegistry(new LinkedHashMap<>());
+
+		registry.register("product", () -> integration.getProductHealth());
+		registry.register("recommendation", () -> integration.getRecommendationHealth());
+		registry.register("review", () -> integration.getReviewHealth());
+
+		return new CompositeReactiveHealthIndicator(healthAggregator, registry);
 	}
 
 	public static void main(String[] args) {
